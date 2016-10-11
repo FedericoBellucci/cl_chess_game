03b94172700f6bfa648f2fdd165e0c761fb45e7a
Roberto Pando <roberto_pando@hotmail.com>
2016-10-11 15:53:32 +0200

    method to turn grid position to board coordinate

---
+++ lib/chess_tools.rb
@@ -0,0 +1,12 @@
+module ChessTools
+
+	def position #turns the grid value into the true board array coordinate row/index
+		placement = ["a", "b", "c", "d", "e", "f", "g", "h"]
+		coordinate = self.split("")
+		coordinate[1] = coordinate[1].to_i - 1
+		placement.each_with_index { |x, i| coordinate[0] = i if coordinate[0] == x }
+		coordinate[0], coordinate[1] = coordinate[1], coordinate[0]
+	end
+
+
+end
