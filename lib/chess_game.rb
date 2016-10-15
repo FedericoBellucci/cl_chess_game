440d20109abe05aa44e6e974afd97faca0d4024a
Roberto Pando <roberto_pando@hotmail.com>
2016-10-15 15:34:25 +0200

    Game complete with saving method

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -1,9 +1,11 @@
 require_relative 'player'
 require_relative 'chess_board'
+require 'yaml'
 
 class ChessGame 
 
 	def initialize
+		load if Dir.glob("bin/*").size > 0
 		@players = whos_playing?
 		@board = ChessBoard.new
 		@turn = 0
@@ -29,6 +31,9 @@ class ChessGame
 		begin
 			print "Choose the piece you wish to move: "
 			piece_from = gets.strip
+			if piece_from == 'save'
+				save
+			end
 		end until @hor_guide.include?(piece_from[0]) && @ver_guide.include?(piece_from[1])
 
 		begin
@@ -70,18 +75,38 @@ class ChessGame
 		puts "* Each turn the game will show whos turn is it."
 		puts "* The player must choose a valid piece to move considering the grid (e.g. 'a2')"
 		puts "* The player must then choose where he wished to move that piece (e.g. 'a3')"
+		puts "* Type 'save' anytime before choosing a move to save the game."
 		puts "	GOOD LUCK! "
 		board
 	end
 	
-	def save(game)
-		saving = File.open('lib/saved_hangman.yaml', 'w')
-		saving.write(YAML::dump(game))
+	def save
+		print "Name the file you want to save: "
+		file_name = gets.strip.downcase.split(" ").join("_")
+		if File.exists?("bin/#{file_name}.yaml")
+			puts "That file name already exist!"
+			save
+		else
+			saving = File.open("bin/#{file_name}.yaml", 'w')
+			saving.write(YAML::dump(self))
+			exit
+		end
 	end
 	
 	def load
-		saved_game = File.open('lib/saved_hangman.yaml', 'r') { |file| file.read }
-		YAML.load saved_game
+		puts "You have saved games, choose from the list to load a game or type 'new' for a new game."
+		Dir.entries("bin").each { |f| puts f.split(".yaml"); puts ""}
+		file_name = gets.strip.downcase
+		if file_name == 'new'
+			puts "New Chess Game"
+		elsif File.exists?("bin/#{file_name}.yaml")
+			saved_game = File.open("bin/#{file_name}.yaml", 'r') { |file| file.read }
+			game = YAML.load saved_game
+			game.board
+		else
+			puts "The file you requested does not exits!"
+			load
+		end
 	end
 end
 ChessGame.new
