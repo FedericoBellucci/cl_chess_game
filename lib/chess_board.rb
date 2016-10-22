f57dd91dbd3601538b0567f02f22566ab28d1dc8
Pablo Crivella <pablocrivella@gmail.com>
2016-10-22 10:34:42 +0200

    Fix rubocop offense Style/IndentationWidth

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -343,11 +343,11 @@ private
 
     @board.each_with_index do |row, num| 
       row.each_with_index do |column, i|
-      if column.color == king.color && column != king
-        enemy_path.each do |x|
-          return false if valid_move?([num, i], x, king.color)
-        end
-        end
+        if column.color == king.color && column != king
+          enemy_path.each do |x|
+            return false if valid_move?([num, i], x, king.color)
+          end
+          end
       end
     end
     true
